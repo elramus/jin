c7e0218c797602f60b09f5796ca2e67e8f36c905
Luke Ramus <luke.ramus@gmail.com>
2018-04-10 08:00:23 -0500

    finish about page

--- page.php
+++ page.php
@@ -6,8 +6,8 @@
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<div class="inner-wrap content">
-				<h3 class='page-title'>
+			<div class="inner-wrap-oversized content">
+				<h3 class='page-title inner-wrap'>
 					<?php if (get_field('top_headline')) {
 						echo get_field('top_headline');
 					} else {
@@ -17,17 +17,19 @@
 				<div class="featured-image-container">
 					<?php the_post_thumbnail(); ?>
 				</div>
-				<?php the_content(); ?>
-				<div class="links">
-					<?php if (get_field('resume')) { ?>
-						<a href="<?php echo get_field('resume')['url']; ?>"><i class="fas fa-arrow-alt-to-bottom"></i>Resume</a>
-					<?php } ?>
-					<?php if (get_field('linkedin')) { ?>
-						<a href="<?php echo get_field('linkedin'); ?>" target="_blank"><i class="fab fa-linkedin"></i></a>
-					<?php } ?>
-					<?php if (get_field('email')) { ?>
-						<a href="mailto:<?php echo get_field('email'); ?>"><i class="fas fa-envelope"></i></a>
-					<?php } ?>
+				<div class="inner-wrap">
+					<?php the_content(); ?>
+					<div class="links">
+						<?php if (get_field('resume')) { ?>
+							<a href="<?php echo get_field('resume')['url']; ?>"><i class="fas fa-arrow-alt-to-bottom"></i>Resume</a>
+						<?php } ?>
+						<?php if (get_field('linkedin')) { ?>
+							<a href="<?php echo get_field('linkedin'); ?>" target="_blank"><i class="fab fa-linkedin"></i></a>
+						<?php } ?>
+						<?php if (get_field('email')) { ?>
+							<a href="mailto:<?php echo get_field('email'); ?>"><i class="fas fa-envelope"></i></a>
+						<?php } ?>
+					</div>
 				</div>
 			</div>
 
